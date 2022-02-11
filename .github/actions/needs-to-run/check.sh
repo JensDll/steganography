dca40174c8ef4242364f978e88d06053e162ea7c
jens <jens@doellmann.com>
2022-02-11 11:18:47 +0100

    auto push

--- .github/actions/needs-to-run/check.sh
+++ .github/actions/needs-to-run/check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-result=$(git diff --name-only "$PR_BASE_SHA" HEAD)
-readonly result
+# result=$(git diff --name-only "$PR_BASE_SHA" HEAD)
+# readonly result
 
-env
+# env
 
+echo "#####################"
+git log --oneline
 echo "The HEAD SHA from env is: $PR_BASE_SHA"
 echo "The HEAD SHA is: $(git rev-parse HEAD)"
 echo "The project passed is: $1"
-echo "The diff is:"
-echo "$result"
