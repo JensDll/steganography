4b49b59a3da72135a703dac2517541bf3eac40b8
jens <jens@doellmann.com>
2022-02-11 01:02:43 +0100

    update

--- .github/actions/needs-to-run/check.sh
+++ .github/actions/needs-to-run/check.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# result=$(git diff --name-only "$PR_BASE_SHA" HEAD)
-# readonly result
+result=$(git diff --name-only "$PR_BASE_SHA" HEAD)
+readonly result
 
-# echo "$result"
-echo "The HEAD SHA from env is $PR_BASE_SHA"
-echo "The HEAD SHA is $(git rev-parse HEAD)"
-echo "The project passed is $1"
+env
+
+echo "The HEAD SHA from env is: $PR_BASE_SHA"
+echo "The HEAD SHA is: $(git rev-parse HEAD)"
+echo "The project passed is: $1"
+echo "The diff is:"
+echo "$result"
