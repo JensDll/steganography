c5991e726cc588bcd051a607983c298c798b54d1
jens <jens@doellmann.com>
2022-02-12 01:22:18 +0100

    auto push

--- .github/actions/needs-to-run/check.sh
+++ .github/actions/needs-to-run/check.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# result=$(git diff --name-only "$PR_BASE_SHA" HEAD)
-# readonly result
+git fetch --depth=1 origin +refs/heads/"$GITHUB_BASE_REF":refs/remotes/origin/"$GITHUB_BASE_REF"
+git branch --track "$GITHUB_BASE_REF" origin/"$GITHUB_BASE_REF"
 
-# env
+while [ -z "${mergeBase:=$(git merge-base "$GITHUB_BASE_REF" HEAD)}" ]
+do     
+  git fetch --deepen=5 origin "$GITHUB_BASE_REF"
+done
 
-echo "#####################"
-git log --oneline
-echo "The HEAD SHA from env is: $PR_BASE_SHA"
-echo "The HEAD SHA is: $(git rev-parse HEAD)"
-echo "The project passed is: $1"
+echo "The project to search for is: $1"
+echo "The head is: $(git rev-parse HEAD)"
+echo "The merge base is: $mergeBase"
+echo "The diff is:"
+git diff --name-only HEAD "$mergeBase"
+
+echo "::set-output name=result::true"
