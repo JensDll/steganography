944ae6f01921af10b00bc5de5f32d79198516e29
jens <jens@doellmann.com>
2022-03-25 14:04:45 +0100

    remove unused if

--- services/api/src/Domain/Entities/AesCounterMode.cs
+++ services/api/src/Domain/Entities/AesCounterMode.cs
@@ -71,9 +71,6 @@ public class AesCounterMode : IDisposable
         {
             uint* counter = (uint*) bytePointer;
             ++*counter;
-            if (*counter == 255)
-            {
-            }
         }
 
         _keyStreamIdx = 0;
