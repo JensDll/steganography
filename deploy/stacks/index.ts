5637a75d6d1a9af7b49b48d71c3007eb425d38aa
jens <jens@doellmann.com>
2022-04-06 23:48:03 +0200

    wip

---
+++ deploy/stacks/index.ts
@@ -0,0 +1,1 @@
+export { AppStack } from './appStack'
--- services/web/src/App.vue
+++ services/web/src/App.vue
@@ -1,6 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const uri = ref('')
+
+async function handleFetch() {
+  const result = await fetch(uri.value)
+  console.log(result)
+  const txt = await result.text()
+  console.log(txt)
+}
+</script>
 
 <template>
   <TheHeader />
   <TheMain />
+  <div class="p-16">
+    <input v-model="uri" class="block w-1/2 border p-4" />
+    <AppButton class="mt-4" variant="encode" @click="handleFetch">
+      Fetch
+    </AppButton>
+  </div>
 </template>
