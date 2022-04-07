3c5f710a96129cbbc28c5a450d5cb78082193849
jens <jens@doellmann.com>
2022-04-08 00:25:21 +0200

    register load balancer and corresponding DNS ARecord

--- deploy/index.ts
+++ deploy/index.ts
@@ -1,13 +1,13 @@
 import 'source-map-support/register'
 import * as cdk from 'aws-cdk-lib'
-import { AppStack } from './stacks'
+import { MainStack } from './src/stacks'
 
 const app = new cdk.App()
 
-new AppStack(app, 'App', {
+new MainStack(app, 'Main', {
   env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION
+    region: process.env.CDK_DEFAULT_REGION,
+    account: process.env.CDK_DEFAULT_ACCOUNT
   }
 })
 
