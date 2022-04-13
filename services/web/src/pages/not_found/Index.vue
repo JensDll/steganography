0bb7b91f1d6966d91e090044ca81129ea8ee9223
jens <jens@doellmann.com>
2022-04-14 01:40:50 +0200

    add not found component

---
+++ services/web/src/pages/not_found/Index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="container">
+    <h2 class="mb-5">Not Found</h2>
+    <p>Sorry, it looks like the requested resource does not exist.</p>
+    <AppButton class="mt-8" variant="encode" @click="$router.back()">
+      Go back
+    </AppButton>
+  </section>
+</template>
+
+<style scoped></style>
