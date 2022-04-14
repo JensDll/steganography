94ae26357fa45d41683283abd0bbc7303f1a5e2d
jens <jens@doellmann.com>
2022-04-14 17:07:12 +0200

    fix duplication in aes counter mode

--- services/web/src/components/app/AppSection.vue
+++ services/web/src/components/app/AppSection.vue
@@ -14,7 +14,7 @@
 }
 
 .dark .section {
-  box-shadow: 0 2px 20px 0 rgb(theme('colors.rgb.gray.700') / 0.8);
+  box-shadow: 0 2px 20px 0 rgb(theme('colors.rgb.black') / 0.55);
 }
 
 @screen section {
@@ -24,7 +24,7 @@
   }
 
   .dark .section {
-    box-shadow: 4px 4px 40px 0 rgb(theme('colors.rgb.gray.700') / 0.4);
+    box-shadow: 4px 4px 40px 0 rgb(theme('colors.rgb.black') / 0.55);
   }
 }
 </style>
