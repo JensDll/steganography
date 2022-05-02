f07a8748c15bf4922dc5827135e4de7174fbdfda
jens <jens@doellmann.com>
2022-05-02 16:26:28 +0200

    move configs

--- deploy/scaleway-k8s/overlays/dev/create-ca.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
-  -sha256 -extensions v3_ca \
-  -out "$DIR/cert.pem" -keyout "$DIR/key.pem"  \
-  -subj "/O=Dev CA"
