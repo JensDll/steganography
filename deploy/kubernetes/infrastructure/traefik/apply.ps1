a64b6e8be3b168e04382502b5a4a5919b5e16a56
jens <jens@doellmann.com>
2022-07-03 14:55:22 +0200

    add buffering middleware to traefik

--- deploy/kubernetes/infrastructure/traefik/apply.ps1
+++ deploy/kubernetes/infrastructure/traefik/apply.ps1
@@ -1,7 +1,7 @@
 ﻿[CmdletBinding()]
 param (
   [Parameter(Position = 0, Mandatory)]
-  [ValidateSet('install', 'delete')]
+  [ValidateSet('install', 'delete', 'dashboard')]
   [string]$Action,
 
   [Parameter(ValueFromRemainingArguments)]
@@ -12,6 +12,12 @@ $namespace = 'traefik'
 
 switch ($Action) {
   install {
+    if ($HelmArgs -contains '--dry-run') {
+      kubectl apply -f "$PSScriptRoot/middleware.yaml" --namespace=$namespace --dry-run=client
+    } else {
+      kubectl apply -f "$PSScriptRoot/middleware.yaml" --namespace=$namespace
+    }
+
     helm upgrade traefik traefik/traefik --install --namespace=$namespace `
       --create-namespace `
       --values="$PSScriptRoot/values.yaml" $HelmArgs
@@ -19,8 +25,16 @@ switch ($Action) {
   delete {
     helm delete traefik --namespace=$namespace $HelmArgs
   
-    if (-not ($HelmArgs -contains '--dry-run')) {
+    if ($HelmArgs -contains '--dry-run') {
+      kubectl delete -f "$PSScriptRoot/middleware.yaml" --namespace=$namespace --dry-run=client
+      kubectl delete namespace $namespace --dry-run=client
+    } else {
+      kubectl delete -f "$PSScriptRoot/middleware.yaml" --namespace=$namespace
       kubectl delete namespace $namespace
     }
+  }
+  dashboard {
+    $podName = kubectl get pods --selector "app.kubernetes.io/name=traefik" --output=name --namespace=$namespace
+    kubectl port-forward $podName 9000:9000 --namespace=$namespace
   }
 }
